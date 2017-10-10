259560ce86c4baa40b3f10e61697b2812efcb76b
lonac <barakamalonao@yahoo.com>
2017-10-11 00:02:17 +0300

    modified:   resources/views/schools/pdf.blade.php

--- resources/views/schools/pdf.blade.php
+++ resources/views/schools/pdf.blade.php
@@ -1,22 +1,26 @@
+<!DOCTYPE html>
 <html>
-		<div class="row">
-				@include('studio._school_properties')
-		</div>
+	<div class="row">
+		@include('studio._school_properties')
+	</div>
+	@if($excel->count()>0)
+		@foreach($excel as $data)
 
-		<div class="row">
-			@if($excel->count()>0)
-				@foreach($excel as $data)
-				<div class="col-md-2 col-md-offset-0">
-					<center>
-						<img src="{{$data->school_id}}/pictures/{{$data->idno}}.jpg" width="140" height="120"><br>
-						{{$data->schools->center_number}}-{{$data->idno}}- {{$data->firstname}} {{$data->middlename}} {{$data->surname }} <br>
-						SEX: {{$data->sex}} <br>
-						................................... <br>
-					</center>	
-				</div>			
-				@endforeach
-			@endif
-		</div>
+			<figure>
+					<img src="{{$data->school_id}}/pictures/{{$data->idno}}.jpg" width="140" height="120"><br>
+				<figcaption>
+					{{$data->schools->center_number}}-{{$data->idno}}- {{$data->firstname}} {{$data->middlename}} {{$data->surname }} <br>
+					SEX: {{$data->sex}} <br>
+					................................... <br>
+				</figcaption>
+			</figure>
+		</td>	
+		</tr>
+		@endforeach
+	@endif	
 </html>
+
+
+
 
 		
