b49ad437864f7b167804616a9f3db95358625db4
lonac <barakamalonao@yahoo.com>
2017-10-11 15:31:37 +0300

    modified:   config/snappy.php

--- config/snappy.php
+++ config/snappy.php
@@ -5,14 +5,16 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'binary'  => '/usr/local/bin/wkhtmltopdf-amd64',
+        //'binary'  => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltoimage',
+         'binary'  => '/usr/local/bin/wkhtmltoimage-amd64',
+        //'binary'  => '/usr/local/bin/wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
